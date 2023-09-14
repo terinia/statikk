72cff6c0138bbed45f3146f777bad0855b54f2da
Balint Biro <rockbag123@gmail.com>
2023-09-14 13:55:22 +0200

    Some code cleanup

--- src/statikk/models.py
+++ src/statikk/models.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import uuid
-from typing import Optional, List, TypeVar, Generic, Any
+from typing import Optional, List, TypeVar, Generic, Any, Set, Type
 
 from pydantic import BaseModel, Field, model_serializer, model_validator
 from pydantic_core._pydantic_core import PydanticUndefined
+from pydantic.fields import FieldInfo
 
 
 class GSI(BaseModel):
@@ -49,6 +50,24 @@ class DatabaseModel(BaseModel):
     id: str
     type: str = None
 
+    @staticmethod
+    def _index_types() -> Set[Type[Index]]:
+        return {Index, IndexPrimaryKeyField, IndexSecondaryKeyField}
+
+    @staticmethod
+    def _is_index_field(field: FieldInfo) -> bool:
+        annotation_bases = set(field.annotation.__bases__) if field.annotation else {}
+        index_types = {Index, IndexPrimaryKeyField, IndexSecondaryKeyField}
+        return len(index_types.intersection(annotation_bases)) > 0
+
+    @classmethod
+    def _create_index_field_from_shorthand(cls, field: FieldInfo, value: str) -> FieldInfo:
+        annotation = field.annotation
+        extra_fields = dict()
+        if field.default is not PydanticUndefined:
+            extra_fields["index_names"] = field.default.index_names
+        return annotation(value=value, **extra_fields)
+
     @model_validator(mode="before")
     @classmethod
     def check_boxed_indexes(cls, data: Any) -> Any:
@@ -65,19 +84,12 @@ class DatabaseModel(BaseModel):
                 data["id"] = str(uuid.uuid4())
             if "type" not in data:
                 data["type"] = cls.__name__
+
             for key, value in data.items():
-                annotation_bases = (
-                    set(cls.model_fields[key].annotation.__bases__) if cls.model_fields[key].annotation else {}
-                )
-                index_types = {Index, IndexPrimaryKeyField, IndexSecondaryKeyField}
-                if index_types.intersection(annotation_bases) and not isinstance(value, dict):
-                    if isinstance(value, tuple(index_types)):
+                field = cls.model_fields[key]
+                if cls._is_index_field(field) and not isinstance(value, dict):
+                    if isinstance(value, tuple(cls._index_types())):
                         continue
                     else:
-                        field = cls.model_fields[key]
-                        annotation = field.annotation
-                        extra_fields = dict()
-                        if field.default is not PydanticUndefined:
-                            extra_fields["index_names"] = field.default.index_names
-                        data[key] = annotation(value=value, **extra_fields)
+                        data[key] = cls._create_index_field_from_shorthand(field, value)
         return data
