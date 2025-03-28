ac3f159272ed18940d88d55bc6cfd90a59dccbb7
Balint Biro <rockbag123@gmail.com>
2025-03-28 16:46:15 +0100

    Removing perf issue line (again) - don't know how it made its way back

--- src/statikk/engine.py
+++ src/statikk/engine.py
@@ -532,7 +532,6 @@ class Table:
                     continue
                 if value is not None:
                     setattr(item, key, value)
-        item.model_rebuild(force=True)
         return item
 
     def _serialize_item(self, item: DatabaseModel):
